8d3ef8d29649eec66a19e923887415da4037bc70
Peter130848 <56257368+Peter130848@users.noreply.github.com>
2020-10-25 17:14:46 +0800

    Update UVa10127_Ones.cpp

--- UVa10127_Ones.cpp
+++ UVa10127_Ones.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main(){
     int number;
     while (cin >> number) {
+        // the result of first modulo is one except the number is 1
         int remain = (number == 1) ? 0 : 1;
         int now, answer;
+        // 取餘數直到%結果是零
         for (answer = 1; remain; answer++) {
             now = remain * 10 + 1;
             remain = now % number;
