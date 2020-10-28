743c427f3a0b957c80a6a2769961fa50b57ddf6b
Peter130848 <56257368+Peter130848@users.noreply.github.com>
2020-10-28 14:20:17 +0800

    Update UVa10165_Stone_Game.cpp

--- UVa10165_Stone_Game.cpp
+++ UVa10165_Stone_Game.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     int nPiles;
     while (cin >> nPiles && nPiles) {
+        // 0 跟任何數字做 XOR = 原本的數字
         int result = 0;
         while (nPiles--){
             int tmp;
