ebe3fc41ce4e3fce87937f47746efa477fe1500f
Peter130848 <56257368+Peter130848@users.noreply.github.com>
2020-11-08 15:52:54 +0800

    Add files via upload

---
+++ UVa10026_Shoemaker_Problem.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Node
+{
+    double tf;
+    int order;
+};
+
+bool cmp(Node a, Node b)
+{
+    if (a.tf > b.tf)
+    {
+        return true;
+    }
+    else if (a.tf == b.tf)
+    {
+        if (a.order < b.order)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    while (cases--)
+    {
+        int jobAmount;
+        cin >> jobAmount;
+        Node jobs[jobAmount];
+        for (int i = 0; i < jobAmount; i++)
+        {
+            double time, fine;
+            cin >> time >> fine;
+            jobs[i].tf = fine / time;
+            jobs[i].order = i + 1;
+        }
+
+        sort(jobs, jobs + jobAmount, cmp);
+        for (int i = 0; i < jobAmount - 1; i++)
+        {
+            cout << jobs[i].order << " ";
+        }
+        cout << jobs[jobAmount - 1].order << endl;
+
+        if (cases)
+        {
+            cout << endl;
+        }
+    }
+    return 0;
+}
